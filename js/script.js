90270e8cb01febdd508fa29cbc5da0b9faf354b1
UrMama <141613642+Mudipas@users.noreply.github.com>
2023-11-16 16:22:07 +0700

    Update baru

---
+++ js/script.js
@@ -0,0 +1,10 @@
+//
+// ==== typing animation ====
+//
+
+var typed = new Typed(".typing", {
+    strings: ["Web Designer", "Web Developer", "Graphic Designer", "YouTuber"],
+    typeSpeed: 100,
+    BackSpeed: 60,
+    loop: true
+});
